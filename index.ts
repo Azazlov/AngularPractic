413fe1b220beb9f6ded50a4984cfd2c2e57c207c
Azazlov <mfd404mf@gmail.com>
2025-10-01 11:07:06 +0300

    Сделал дз на второе занятие

--- index.ts
+++ index.ts
@@ -1,2 +1,13 @@
-console.log(65536>>16)
-// Типа 1
+function main(){
+    let age: number = Math.floor(Math.random()*100);
+    if (age < 18){
+        console.log('Доступ запрещен')
+        return
+    }
+    else if (age >= 18){
+        for (let i: number = 1; i < 11; i++){
+            console.log(age*i)
+        }
+    }
+}
+main();
