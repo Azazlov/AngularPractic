21d7a1cf31299beb39baf5f6fa54998584288db3
Azazlov <mfd404mf@gmail.com>
2025-10-01 11:07:37 +0300

    Доделал

--- index.ts
+++ index.ts
@@ -1,13 +1,29 @@
+import * as readline from 'readline';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 function main(){
-    let age: number = Math.floor(Math.random()*100);
-    if (age < 18){
-        console.log('Доступ запрещен')
-        return
-    }
-    else if (age >= 18){
-        for (let i: number = 1; i < 11; i++){
-            console.log(age*i)
+
+    rl.question('Введите ваш возраст: ', (input: string) => {
+        rl.close();
+
+        // let age: number = Math.floor(Math.random()*100); 
+        // Рандомный вариант
+
+        let age:number = Number(input);
+        
+        if (age < 18){
+            console.log('Доступ запрещен')
+            return
         }
-    }
+        else if (age >= 18){
+            for (let i: number = 1; i < 11; i++){
+                console.log(`${age} * ${i} = ${age*i}`)
+            }
+        }
+    });
 }
 main();
