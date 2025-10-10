33c45a39e5d5b60b7c6fedda5976846518f63cbf
Azazlov <mfd404mf@gmail.com>
2025-10-10 10:25:34 +0300

    4 урок ангуляр выполнен

--- cnt-practic/src/app/app.module.ts
+++ cnt-practic/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PagesComponent } from './pages/pages.component';
+import { HeaderComponent } from './@theme/components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
